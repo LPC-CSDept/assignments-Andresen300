9661debbaf7328ab19fd651baa8d5e043e2fabff
Victoria Andresen <77986639+Andresen300@users.noreply.github.com>
2021-09-15 02:12:31 +0000

    W

--- ch8/assn8-3.cpp
+++ ch8/assn8-3.cpp
@@ -8,12 +8,12 @@ int main()
 {
 	const int N = 10;
 	int numbers[N] = {25, 10, 15, 30, 35, 40, 45, 55,20,50};
+	int minidx;
 
 	printArray(numbers, N);
 
 	for(int i=0;i<N-1;i++)
     {
-        int minidx;
         minidx= findMin(numbers, N, i); // Find the i_th smallest number
 		swap(numbers[i], numbers[minidx]);
 	}
@@ -24,10 +24,21 @@ int main()
 
 int findMin(int numbers[], int N, int ith)
 {
-
-	return ith;
+	int min, minidx;
+	min = numbers[ith];
+    for(int i=ith ;i<N;i++)
+	{
+		if ( min > numbers[i] )
+		{
+			min = numbers[i];
+			minidx = i;
+		}
+	}
+	return i; // 1
 }
-int printArray(int [], int)
+
+int printArray(int [], int x)
 {
-	
+	for(int i = 0; i < int x; i++)
+  
 }
